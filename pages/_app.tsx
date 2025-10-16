b2447bb109da9cd1d67181cf43acf749d9ecdda8
santosh yadav <santoshyadav2638@gmail.com>
2025-10-16 13:42:31 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
-
+#export the default
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
